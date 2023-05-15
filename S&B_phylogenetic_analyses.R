a767a5e84697117cc3002979a38b4bb42b4d5c0f
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2023-05-15 15:22:16 -0700

    Update S&B_phylogenetic_analyses.R

--- S&B_phylogenetic_analyses.R
+++ S&B_phylogenetic_analyses.R
@@ -1,1 +1,12 @@
 ####phylogenetic analyses for Ch 1 
+
+setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18")
+
+library(ape)
+install.packages("geiger")
+library(geiger)
+
+#import S&B18 tree and check data 
+SBtree <- read.tree(file = "ALLMB.tre")
+write.tree(SBtree)
+is.rooted(SBtree)
