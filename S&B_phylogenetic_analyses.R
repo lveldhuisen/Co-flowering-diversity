ee7127b61dd4900e097924449a2d222fe5bbb56b
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2023-05-23 13:05:24 -0600

    Update S&B_phylogenetic_analyses.R

--- S&B_phylogenetic_analyses.R
+++ S&B_phylogenetic_analyses.R
@@ -22,7 +22,7 @@ matrix2021 <- read.table("2021_community_matrix.txt", sep = "\t", header = T, ro
 
 matrix2021[1, matrix2021[1,]>0]
 
-#richness of PBM 2021
+######richness of PBM 2021###################################################
 richnessPBM21 = length(matrix2021[1, matrix2021[1,]>0])
 #prune tree to just these species for PBM
 pruned.treePBM21 <- treedata(SBtree, unlist(matrix2021[1,matrix2021[1,]>0]), warnings = F)$phy
@@ -33,11 +33,20 @@ plot(pruned.treePBM21)
 #Faith's index for PBM2021
 sum(pruned.treePBM21$edge.length)
 
-#prune tree to just these species for Pfeiler
+###prune tree to just these species for Pfeiler###############################
 pruned.treePf21 <- treedata(SBtree, unlist(matrix2021[2,matrix2021[2,]>0]), warnings = F)$phy
 class(pruned.treePf21)
 pruned.treePf21
 plot(pruned.treePf21)
 
 #Faith's index for Pfeiler2021
-sum(pruned.treePBM21$edge.length)
+sum(pruned.treePf21$edge.length)
+
+###prune tree to just these species for road 2021##############################
+pruned.treeroad21 <- treedata(SBtree, unlist(matrix2021[3,matrix2021[3,]>0]), warnings = F)$phy
+class(pruned.treeroad21)
+pruned.treeroad21
+plot(pruned.treeroad21)
+
+#Faith's index for Road 2021
+sum(pruned.treePf21$edge.length)
