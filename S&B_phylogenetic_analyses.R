b2a291a4c454dc7b9d7800d97a5df191445e3940
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2023-05-23 08:09:55 -0600

    Update S&B_phylogenetic_analyses.R

--- S&B_phylogenetic_analyses.R
+++ S&B_phylogenetic_analyses.R
@@ -11,3 +11,7 @@ SBtree <- read.tree(file = "ALLMB.tre")
 write.tree(SBtree)
 is.rooted(SBtree)
 
+##2021 community matrix
+setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
+
+matrix2021 <- read.table("2021_community_matrix.txt", sep = "\t", header = T, row.names = 1)
