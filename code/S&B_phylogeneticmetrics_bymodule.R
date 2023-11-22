7ff5296e9a5edafc89fdf0ec08436998d96ae395
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2023-11-22 11:28:23 -0700

    Update S&B_phylogeneticmetrics_bymodule.R

--- code/S&B_phylogeneticmetrics_bymodule.R
+++ code/S&B_phylogeneticmetrics_bymodule.R
@@ -1,67 +1,63 @@
-##Phylogenetic metrics by module
+##Phylogenetic metrics by module (network analysis output), all necessary files are in the "files_phylogeneticmetrics_bymodule" folder
+
+
+install.packages("ape")
+install.packages("geiger")
+install.packages("picante")
 library(ape)
 library(geiger)
 library(picante)
 
 
-#import S&B18 tree and check data 
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18")
+#import Smith and Brown 2018 tree and check data 
+#file available in "files_phylogeneticetrics_bymodule" folder
 SBtree <- read.tree(file = "ALLMB.tre")
 
 #community matrix with my data 
 setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
 matrix2021.mod <- read.table("2021_community_matrix_modules.txt", sep = "\t", header = T, row.names = 1)
 
-
 #Faith's PD-------------------------------------------------------------------
 ##2021#################
 prune.sum.function <- function(x){
   tmp.tree <- treedata(SBtree, x[x>0])$phy
   sum(tmp.tree$edge.length)
-}
+} #function to calculate PD from phylogeny 
 
-PD21.mod <- apply(matrix2021.mod, MARGIN = 1, prune.sum.function)
-print(PD21.mod)
+PD21.mod <- apply(matrix2021.mod, MARGIN = 1, prune.sum.function) #save PD values by module
+print(PD21.mod) #view PD values 
 PD21.mod
 
 ###SES###############################
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
 
 pruned.tree2021 <- treedata(SBtree, unlist(matrix2021.mod[10,matrix2021.mod[10,]>0]), warnings = F)$phy
-plot(pruned.tree2021)
+plot(pruned.tree2021) #prune tree
 
 #standard effect size using picante 
-library(picante)
 ses.pd(matrix2021.mod, pruned.tree2021, null.model = c("sample.pool"),
-       runs = 5000, iterations = 5000, include.root=TRUE)
+       runs = 5000, iterations = 5000, include.root=TRUE) #output shows PD and SES for all modules for 2021
 
 ##2022#####################################
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
 matrix2022.mod <- read.table("2022_community_matrix_modules.txt", sep = "\t", header = T, row.names = 1)
 
-pruned.tree2022 <- treedata(SBtree, unlist(matrix2022.mod[11,matrix2022.mod[11,]>0]), warnings = F)$phy
-plot(pruned.tree2022)
+pruned.tree2022 <- treedata(SBtree, unlist(matrix2022.mod[11,matrix2022.mod[11,]>0]), warnings = F)$phy #prune tree
+plot(pruned.tree2022) #check tree 
 
 prune.sum.function <- function(x){
   tmp.tree <- treedata(pruned.tree2022, x[x>0])$phy
   sum(tmp.tree$edge.length)
-}
+} #function to calculate PD from phylogeny
 
-PD22.mod <- apply(matrix2022.mod, MARGIN = 1, prune.sum.function)
-print(PD22.mod)
-PD22.mod
+PD22.mod <- apply(matrix2022.mod, MARGIN = 1, prune.sum.function) #save values
+print(PD22.mod) #view values 
 
 ###SES 2022##########
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
 
 ses.pd(matrix2022.mod, pruned.tree2022, null.model = c("sample.pool"),
-       runs = 5000, iterations = 5000, include.root=TRUE)
+       runs = 5000, iterations = 5000, include.root=TRUE) #output shows PD and SES for all modules for 2022
 
 
 #MPD--------------------------------------------------------------
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18")
-SBtree <- read.tree(file = "ALLMB.tre")
-
 ##2021#########
 
 pruned.tree2021 <- treedata(SBtree, unlist(matrix2021[4,matrix2021[4,]>0]), warnings = F)$phy
@@ -74,32 +70,29 @@ new.mpd.function <- function(x){
   mean(as.dist(dist.mat2021[com.names,com.names]))
 }
 
-mpd_2021 <- apply(matrix2021.mod, MARGIN = 1, new.mpd.function)
-print(mpd_2021)
+mpd_2021 <- apply(matrix2021.mod, MARGIN = 1, new.mpd.function) #save MPD values for all modules
+print(mpd_2021) #view MPD values
 
 ###SES MPD 2021#########
 ses.mpd(matrix2021.mod, dist.mat2021, null.model = c("sample.pool"),
-        abundance.weighted = FALSE, runs = 5000, iterations = 5000)
+        abundance.weighted = FALSE, runs = 5000, iterations = 5000) #output shows MPD values and SES for all modules in all 2021 sites 
 
 ##2022#########
 
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 1")
-matrix2022.mod <- read.table("2022_community_matrix_modules.txt", sep = "\t", header = T, row.names = 1)
-
-dist.mat2022 <- cophenetic(pruned.tree2022)
-dist.mat2022
+dist.mat2022 <- cophenetic(pruned.tree2022) #make 2022 distane matrix from pruned phylogeny 
+dist.mat2022 #check matrix 
 #function for all modules at once
 new.mpd.function <- function(x){
   com.names<- names(x[x>0])
   mean(as.dist(dist.mat2022[com.names,com.names]))
 }
 
-mpd_2022 <- apply(matrix2022.mod, MARGIN = 1, new.mpd.function)
-print(mpd_2022)
+mpd_2022 <- apply(matrix2022.mod, MARGIN = 1, new.mpd.function) #save all MPD values
+print(mpd_2022) #view MPD values
 
 ###SES MPD 2022######
 ses.mpd(matrix2022.mod, dist.mat2022, null.model = c("sample.pool"),
-        abundance.weighted = FALSE, runs = 5000, iterations = 5000)
+        abundance.weighted = FALSE, runs = 5000, iterations = 5000) #output shows MPD and SES for all modules in all sites for 2022
 
 #MNTD------------------------------------------------------------------------
 ##2021###############
@@ -110,11 +103,12 @@ new.mntd.function <- function(x){
   mean(apply(my.com.dist, MARGIN = 1, min), na.rm=TRUE)
 }
 
-mntd.2021.mod <- apply(matrix2021.mod, MARGIN = 1, new.mntd.function)
-mntd.2021.mod
+mntd.2021.mod <- apply(matrix2021.mod, MARGIN = 1, new.mntd.function) #save MNTD values for all modules in all sites for 2021
+mntd.2021.mod #view values 
+
 ###SES 2021 MNTD###########
 ses.mntd(matrix2021.mod, dist.mat2021, null.model = c("sample.pool"),
-         abundance.weighted=FALSE, runs = 5000, iterations = 5000)
+         abundance.weighted=FALSE, runs = 5000, iterations = 5000) #shows MNTD values and SES for all modules in all sites for 2021
 
 ##2022#############
 new.mntd.function <- function(x){
@@ -124,9 +118,10 @@ new.mntd.function <- function(x){
   mean(apply(my.com.dist, MARGIN = 1, min), na.rm=TRUE)
 }
 
-mntd.2022.mod <- apply(matrix2022.mod, MARGIN = 1, new.mntd.function)
-mntd.2022.mod
+mntd.2022.mod <- apply(matrix2022.mod, MARGIN = 1, new.mntd.function) #save MNTD values for all modules in all sites for 2022
+mntd.2022.mod #view values 
+
 ###SES 2022 MNTD############
 ses.mntd(matrix2022.mod, dist.mat2022, null.model = c("sample.pool"),
-         abundance.weighted=FALSE, runs = 5000, iterations = 5000)
+         abundance.weighted=FALSE, runs = 5000, iterations = 5000) #shows MNTD values and SES for all modules in all sites for 2022
 
