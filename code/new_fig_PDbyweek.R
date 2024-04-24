513333b8431d880dfc481a4414626b72c5ea1a9d
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-04-24 10:46:37 -0700

    Create new_fig_PDbyweek.R

---
+++ code/new_fig_PDbyweek.R
@@ -0,0 +1,16 @@
+#file includes analysis for PD, MPD and MNTD by week of the flowering season 
+#and code for the new figure with these results 
+
+
+library(ggplot2)
+library(dplyr)
+library(ggpubr)
+library(viridis)
+library(pals)
+library(Polychrome)
+library(picante)
+
+#import Smith & Brown 2018 tree and check data 
+SBtree <- read.tree(file = "ALLMB.tre")
+write.tree(SBtree) #test if imported correctly, can take a long time though 
+is.rooted(SBtree) #check tree, should say true 
